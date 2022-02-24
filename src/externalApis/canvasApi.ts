bfa4b76652a6d11386a2c20b600a008754fbbefa
Carlos Saito <carlos@saito.style>
2022-02-24 09:36:41 +0100

    Add libs for Ladok API and Canvas API

---
+++ src/externalApis/canvasApi.ts
@@ -0,0 +1,61 @@
+/**
+ * This module contains functions to call Canvas API.
+ * Functions do not contain any logic
+ */
+import Canvas from "@kth/canvas-api";
+
+const canvas = new Canvas(
+  process.env.CANVAS_API_URL!,
+  process.env.CANVAS_API_TOKEN!
+);
+
+export interface Section {
+  sis_section_id: string;
+  integration_id: string;
+  name: string;
+}
+
+export interface Assignment {
+  id: number;
+  name: string;
+  grading_type: "gpa_scale" | "points" | "letter_grade";
+  grading_standard_id: number | null;
+  due_at: string | null;
+  unlock_at: string | null;
+  lock_at: string | null;
+}
+
+export interface Submission {
+  id: number;
+  grade: string | null;
+  score: number | null;
+  graded_at: string | null;
+  submitted_at: string | null;
+}
+
+export interface Enrollment {
+  grades: {
+    unposted_current_grade: string;
+  };
+  user: {
+    integration_id: string;
+  };
+}
+
+export function getCanvasSections(courseId: string) {
+  return canvas.listItems<Section>(`courses/${courseId}/sections`);
+}
+
+export function getAssignments(courseId: string) {
+  return canvas.listItems<Assignment>(`courses/${courseId}/assignments`);
+}
+
+export function getSubmissions(courseId: string, assignmentId: string) {
+  return canvas.listItems<Submission>(
+    `courses/${courseId}/assignments/${assignmentId}/submissions`
+  );
+}
+
+export function getFinalGrades(courseId: string) {
+  return canvas.listItems<Enrollment>(`courses/${courseId}/enrollments`);
+}
---
+++ src/externalApis/ladokApi.ts
@@ -0,0 +1,167 @@
+/**
+ * This module are functions to call Ladok API. They do not contain any logic
+ */
+import got from "got";
+
+const gotClient = got.extend({
+  prefixUrl: process.env.LADOK_API_BASEURL,
+  headers: {
+    Accept: "application/vnd.ladok-resultat+json",
+  },
+  responseType: "json",
+  https: {
+    pfx: Buffer.from(process.env.LADOK_API_PFX_BASE64 as string, "base64"),
+    passphrase: process.env.LADOK_API_PFX_PASSPHRASE,
+  },
+});
+
+export interface Resultat {
+  Betygsgrad: number;
+  BetygsskalaID: number;
+  Examinationsdatum: string;
+}
+
+export interface Kurstillfalle {
+  Benamning: {
+    sv: string;
+    en: string;
+  };
+  Betygsskala: number;
+  KravPaProjekttitel: boolean;
+  Kurstillfalleskod: string;
+  ResultatrapporteringMojlig: boolean;
+  UtbildningsinstansUID: string;
+  Utbildningskod: string;
+  Versionsnummer: number;
+
+  IngaendeMoment: Array<{
+    Benamning: {
+      sv: string;
+      en: string;
+    };
+    BetygsskalaID: number;
+    KravPaProjekttitel: boolean;
+    Omfattning: number;
+    ResultatrapporteringMojlig: boolean;
+    UtbildningsinstansUID: string;
+    Utbildningskod: string;
+  }>;
+}
+
+export interface Aktivitetstillfallesmojlighet {
+  TotalAntalPoster: number;
+  Resultat: Array<{
+    KurstillfalleStudentenArRegistreradPa: string;
+  }>;
+}
+
+export interface SokResultat {
+  TotalAntalPoster: number;
+  Resultat: Array<{
+    Avbrott?: {
+      Avbrottsdatum: string;
+    };
+    Student: {
+      Efternamn: string;
+      Fornamn: string;
+
+      /** Use this ID to match the student in Ladok with same person in Canvas */
+      Uid: string;
+    };
+    /**
+     * This is the "StudieresultatUID" that you need to send to Ladok to create
+     * a result in Ladok
+     */
+    Uid: string;
+
+    /** Present if there is a grade in form of utkast */
+    ResultatPaUtbildningar?: Array<{
+      Arbetsunderlag: {
+        Betygsgradobjekt: {
+          ID: number;
+          Kod: string;
+        };
+        /**
+         * This is the "ResultatUID" that you need to send to Ladok in order to
+         * update or delete a result
+         */
+        Uid: string;
+      };
+    }>;
+  }>;
+}
+
+export function getAktivitetstillfallesmojlighet(
+  aktivitetstillfalleUID: string
+) {
+  return gotClient.get<Aktivitetstillfallesmojlighet>(
+    `resultat/aktivitetstillfallesmojlighet/aktivitetstillfallesmojlighet/filtrera/utananonymbehorighet?aktivitetstillfalleUID=${aktivitetstillfalleUID}&page=1&limit=400`
+  );
+}
+
+export function getCourseRoundStructure(kurstillfalleUID: string) {
+  return gotClient.get<Kurstillfalle>(
+    `resultat/kurstillfalle${kurstillfalleUID}/moment`
+  );
+}
+
+export function searchModulesStudieresultat(
+  utbildningsinstansUID: string,
+  status: "OBEHANDLADE" | "UTKAST",
+  KurstillfallenUID: string[]
+) {
+  return gotClient.put<SokResultat>(
+    `resultat/studieresultat/rapportera/utbildningsinstans/${utbildningsinstansUID}/sok`,
+    {
+      json: {
+        Filtrering: [status],
+        KurstillfallenUID,
+      },
+    }
+  );
+}
+
+export function searchExaminationsStudieresultat(
+  aktivitetstillfalleUID: string,
+  status: "OBEHANDLADE" | "UTKAST",
+  KurstillfallenUID: string[]
+) {
+  return gotClient.put<SokResultat>(
+    `resultat/studieresultat/rapportera/aktivitetstillfalle/${aktivitetstillfalleUID}/sok`,
+    {
+      json: {
+        Filtrering: [status],
+        KurstillfallenUID,
+      },
+    }
+  );
+}
+
+export function createResult(
+  studieresultatUID: string,
+  utbildningsinstansUID: string,
+  resultat: Resultat
+) {
+  return gotClient.post<any>(
+    `resultat/studieresultat/${studieresultatUID}/utbildning/${utbildningsinstansUID}/resultat`,
+    {
+      json: resultat,
+    }
+  );
+}
+
+export function updateResult(
+  resultatUID: string,
+  newValue: Resultat,
+  currentDate?: Date
+) {
+  return gotClient.put<any>(
+    `resultat/studieresultat/resultat/${resultatUID}/`,
+    {
+      json: {
+        ...newValue,
+        SenasteResultatandring: (currentDate || new Date()).toISOString(),
+      },
+    }
+  );
+}
