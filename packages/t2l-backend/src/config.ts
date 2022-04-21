0ebc9ff3ce7f837216cda86bec8d3bb3b5c9397f
Carlos Saito <carlos@saito.style>
2022-04-21 11:25:34 +0200

    Configure skog

---
+++ packages/t2l-backend/src/config.ts
@@ -0,0 +1,22 @@
+import log, { initializeLogger, setFields } from "skog";
+require("dotenv").config();
+initializeLogger();
+setFields({
+  app: "transfer-to-ladok",
+});
+
+process.on("uncaughtException", (err) => {
+  log.fatal(err, "UncaughtException");
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason) => {
+  if (reason instanceof Error) {
+    log.fatal(reason, "UnhandledRejection");
+  } else {
+    log.fatal(
+      "UnhandledRejection: some promise have been rejected but didn't throw an `Error` object. It is not possible to show a stack trace"
+    );
+  }
+  process.exit(1);
+});
--- packages/t2l-backend/src/index.ts
+++ packages/t2l-backend/src/index.ts
@@ -1,11 +1,14 @@
+import "./config";
 import express from "express";
 import router from "./router";
 import sessionMiddleware from "express-session";
+import log, { skogMiddleware } from "skog";
 
 const app = express();
 const port = 3000;
 
 app.use(sessionMiddleware());
+app.use(skogMiddleware);
 app.use("/transfer-to-ladok", router);
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
