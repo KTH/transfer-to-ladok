c7896f2a0b94ee4495327dcad014e4b7df309d9d
Carlos Saito <carlos@saito.style>
2022-04-21 15:35:13 +0200

    Implement request from Canvas in backend

--- packages/t2l-backend/src/index.ts
+++ packages/t2l-backend/src/index.ts
@@ -1,13 +1,15 @@
 import "./config";
 import express from "express";
 import router from "./router";
-import sessionMiddleware from "express-session";
+// import sessionMiddleware from "express-session";
 import log, { skogMiddleware } from "skog";
 
 const app = express();
 const port = 3000;
 
-app.use(sessionMiddleware());
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(sessionMiddleware());
 app.use(skogMiddleware);
 app.use("/transfer-to-ladok", router);
 app.listen(port, () => {
--- packages/t2l-backend/src/router.ts
+++ packages/t2l-backend/src/router.ts
@@ -3,6 +3,14 @@ import sectionsHandler from "./apiHandlers/sections";
 import auth from "./apiHandlers/auth";
 const router = Router();
 
+router.post("/", (req, res) => {
+  const domain = req.body.custom_domain;
+  const courseId = req.body.custom_courseid;
+
+  //
+
+  res.redirect(`/transfer-to-ladok?courseId=${courseId}`);
+});
 router.use("/auth", auth);
 
 export default router;
