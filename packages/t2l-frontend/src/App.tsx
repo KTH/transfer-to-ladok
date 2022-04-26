531d6f33c48ed49563d66b0cfd265d2dac3ed91b
Carlos Saito <carlos@saito.style>
2022-04-26 08:39:43 +0200

    Query to the frontend

---
+++ packages/t2l-frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useQuery, QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+function Home() {
+  const sections = useQuery("sections", () =>
+    fetch("/api/courses/1/sections").then((r) => r.json())
+  );
+
+  if (sections.data) {
+    return <div>Loaded!!</div>;
+  }
+  return <div>loading...</div>;
+}
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  );
+}
