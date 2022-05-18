e44ecac26dce87c9d75bfb352368c83853eeb7f8
Carlos Saito <carlos@saito.style>
2022-05-18 09:14:49 +0200

    Show gradebook directly if one exam

--- packages/t2l-frontend/src/App.tsx
+++ packages/t2l-frontend/src/App.tsx
@@ -3,16 +3,29 @@ import { ErrorBoundary } from "react-error-boundary";
 import { QueryClient, QueryClientProvider } from "react-query";
 import FullPageError from "./components/FullPageError";
 import { useSections } from "./hooks/apiClient";
+import Gradebook from "./screens/Gradebook";
 import Unauthenticated from "./screens/Unauthenticated";
 
 const queryClient = new QueryClient();
 
+type Destination =
+  | {
+      aktivitetstillfalleId: string;
+    }
+  | {
+      kurstillfalleId: string;
+      utbildningsinstansId: string;
+    };
+
 function Home() {
   const courseId = new URLSearchParams(location.search).get("courseId");
   const query = useSections(courseId);
+  const [destination, setDestination] = React.useState<Destination | null>(
+    null
+  );
 
   if (query.status === "loading") {
-    return <div>loading...</div>;
+    return <div></div>;
   }
 
   if (query.status === "unauthenticated") {
@@ -23,7 +36,23 @@ function Home() {
     throw query.error;
   }
 
-  return <div>loaded!!!</div>;
+  // If there is only one examination, just choose it
+  const { aktivitetstillfalle, kurstillfalle } = query.sections;
+  if (
+    !destination &&
+    aktivitetstillfalle.length === 1 &&
+    kurstillfalle.length === 0
+  ) {
+    setDestination({
+      aktivitetstillfalleId: aktivitetstillfalle[0].id,
+    });
+  }
+
+  if (!destination) {
+    return <div>Selector</div>;
+  }
+
+  return <Gradebook />;
 }
 
 export default function App() {
---
+++ packages/t2l-frontend/src/screens/Gradebook.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Gradebook() {
+  // Load students
+  // Load assignments lists
+
+  // Load results from Canvas and Ladok
+  // Prepare to send
+  return <div>Destination selected</div>;
+}
