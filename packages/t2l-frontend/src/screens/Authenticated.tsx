ff0f6d5d25b4ff0055b5daea4425bdc92aa13604
Carlos Saito <carlos@saito.style>
2022-05-18 11:40:40 +0200

    App for Authenticated users

--- packages/t2l-frontend/src/screens/Authenticated.tsx
+++ packages/t2l-frontend/src/screens/Authenticated.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import type { Sections } from "t2l-backend";
+import type { Sections, AktSection } from "t2l-backend";
 
-interface Props {
-  sections: Sections;
+function AppWithSelector() {
+  // TODO: Implement this
+  return <div>There are many things to choose from</div>;
 }
 
-export default function Authenticated({ sections }: Props) {
-  return <div>Yay!</div>;
+function AppWithoutSelector({ akt }: { akt: AktSection }) {
+  return <div>Transfer to Ladok for {akt.name}</div>;
+}
+
+export default function Authenticated({ sections }: { sections: Sections }) {
+  const { aktivitetstillfalle, kurstillfalle } = sections;
+
+  // If there are no aktivitetstillfälle or kurstillfälle, then
+  // this course cannot be used with Transfer to Ladok
+  if (aktivitetstillfalle.length === 0 && kurstillfalle.length === 0) {
+    throw new Error("Cannot use here!");
+  }
+
+  // If there is only one section and it is a aktivitetstillfälle,
+  // we don't show any selector
+  if (aktivitetstillfalle.length === 1 && kurstillfalle.length === 0) {
+    return <AppWithoutSelector akt={aktivitetstillfalle[0]} />;
+  }
+
+  // Otherwise the user needs to choose a destination
+  return <AppWithSelector />;
 }
