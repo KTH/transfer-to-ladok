b4cf56fa56fb43194f434475dad1aae194e3e050
Carlos Saito <carlos@saito.style>
2022-05-19 20:27:43 +0200

    Sass in Unauthenticated

--- packages/t2l-frontend/src/screens/Unauthenticated.tsx
+++ packages/t2l-frontend/src/screens/Unauthenticated.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Unauthenticated.scss";
 
 interface Params {
   courseId: string;
@@ -7,10 +8,22 @@ interface Params {
 /** Screen shown when the user is not logged in */
 export default function Unauthenticated({ courseId }: Params) {
   return (
-    <div>
-      <h1>Welcome to Transfer to Ladok</h1>
-      <p>Lorem ipsum todo todo</p>
-      <a href={`/transfer-to-ladok/auth?courseId=${courseId}`}>Go!</a>
+    <div className="container">
+      <h1>KTH Transfer to Ladok</h1>
+      <p>
+        This application has been developed with the intent to enable a secure
+        digital transfer of grades from Canvas Gradebook to Ladok and hopefully
+        also reduce the amount of manual grade administration.
+      </p>
+      <div>
+        <a
+          className="btn"
+          href={`/transfer-to-ladok/auth?courseId=${courseId}`}
+        >
+          Launch the application
+        </a>
+        <p>In the next step, you need to authorize the app</p>
+      </div>
     </div>
   );
 }
