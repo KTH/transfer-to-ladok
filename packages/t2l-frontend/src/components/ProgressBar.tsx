5b987d2a2094048f2cb144c824ca170bff934aa2
Carlos Saito <carlos@saito.style>
2022-05-20 16:16:44 +0200

    ProgressBar component

---
+++ packages/t2l-frontend/src/components/ProgressBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ProgressBar.scss";
+
+export function IndeterminateProgressBar() {
+  return (
+    <div className="IndeterminateProgressBar">
+      <div className="track"></div>
+    </div>
+  );
+}
