c0d91a30973a66a36efa85230ada495674d3cb80
Carlos Saito <carlos@saito.style>
2022-05-24 14:42:04 +0200

    FullPageError component

--- packages/t2l-frontend/src/screens/FullPageError.tsx
+++ packages/t2l-frontend/src/screens/FullPageError.tsx
@@ -1,11 +1,25 @@
-import { ErrorInfo } from "react";
+import React, { ErrorInfo } from "react";
 import { FallbackProps } from "react-error-boundary";
-import { ApiError } from "../hooks/apiClient";
+// import { ApiError } from "../hooks/apiClient";
+import "./FullPageError.scss";
 
 export default function FullPageError({ error }: FallbackProps) {
-  if (error instanceof ApiError && error.code === "unauthorized") {
-    return <div>Login page</div>;
-  }
-
-  return <div>WTF</div>;
+  return (
+    <div className="FullPageError">
+      <header>
+        <h1>Invalid course</h1>
+        {/* <div>You cannot use Transfer to Ladok in this course</div> */}
+      </header>
+      <main>
+        <p>
+          You can only use KTH Transfer to Ladok in official courses or
+          examinations. You cannot use it in intern nor sandbox courses.
+        </p>
+        <p className="small">
+          Contact IT-support if you need more help:{" "}
+          <a href="mailto:it-support@kth.se">it-support@kth.se</a>
+        </p>
+      </main>
+    </div>
+  );
 }
