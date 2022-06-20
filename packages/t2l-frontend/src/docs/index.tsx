e3c9679cb63b5172180f79ce42062a48977dd27e
Carlos Saito <carlos@saito.style>
2022-06-20 21:08:35 +0200

    Update documentation

--- packages/t2l-frontend/src/docs/index.tsx
+++ packages/t2l-frontend/src/docs/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import Hello from "./docs/Example.mdx";
+import Hello from "./Example.mdx";
 
 function App2() {
   return <Hello />;
