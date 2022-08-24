d4201b618eadd0533701643ac8b729f2a54f55e7
Carlos Saito <carlos@saito.style>
2022-08-24 16:37:40 +0200

    Style Done page

--- packages/t2l-frontend/src/screens/Done.tsx
+++ packages/t2l-frontend/src/screens/Done.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Done.scss";
 import { GradesTable } from "../components/GradesTable";
 import { RowAfter } from "../utils/getResultsToBeTransferred";
+import { ArrowLeft } from "../utils/icons";
 
 export default function Done({
   ladokUrl = "",
@@ -14,17 +15,29 @@ export default function Done({
     <div className="Done">
       <header>
         <h1>Transfer completed</h1>
-        <p>
-          Now can you{" "}
-          <a href={ladokUrl} target="_blank">
-            go to Ladok to see the results
-          </a>{" "}
-          or go back to transfer more results
-        </p>
       </header>
       <main className="main">
+        <header className="summary">
+          3 results were successfully transferred. 7 results were not
+          transferred (4 of them with errors)
+        </header>
         <GradesTable results={results} />
       </main>
+      <footer>
+        <p>
+          <a href={ladokUrl} target="_blank">
+            Go to Ladok to see the results.
+          </a>{" "}
+          From there you can continue the process: mark grades as ready (
+          <em>klarmarkera</em>), certify (<em>attestera</em>) and make any other
+          adjustments
+        </p>
+
+        <button className="btn-secondary with-icon">
+          <ArrowLeft />
+          <div className="label">Start over</div>
+        </button>
+      </footer>
     </div>
   );
 }
