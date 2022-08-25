bcc164cbc9d33d33e8de910d5d3e3fb23a6fdaf0
Carlos Saito <carlos@saito.style>
2022-08-25 14:11:08 +0200

    Add mock: 0 grades left to grade

--- packages/t2l-frontend/src/mocks/browser.ts
+++ packages/t2l-frontend/src/mocks/browser.ts
@@ -1,5 +1,10 @@
 import { setupWorker } from "msw";
 import mock1Handlers from "./handlers/mock-1";
+import mock2Handlers from "./handlers/mock-2";
 import sf1624Handlers from "./handlers/mock-sf1624";
 // This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...mock1Handlers, ...sf1624Handlers);
+export const worker = setupWorker(
+  ...mock1Handlers,
+  ...mock2Handlers,
+  ...sf1624Handlers
+);
