b01342770ec3987108f5bf9185dfc6c049f7fc19
Carlos Saito <carlos@saito.style>
2023-01-30 13:18:46 +0100

    Fix dev environment

--- packages/t2l-frontend/src/index.tsx
+++ packages/t2l-frontend/src/index.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
-if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mocks/browser");
-  worker.start({
-    serviceWorker: {
-      url: "/transfer-to-ladok/mockServiceWorker.js",
-    },
-  });
-}
+// if (process.env.NODE_ENV === "development") {
+//   const { worker } = require("./mocks/browser");
+//   worker.start({
+//     serviceWorker: {
+//       url: "/transfer-to-ladok/mockServiceWorker.js",
+//     },
+//   });
+// }
 
 render(<App />, document.getElementById("root"));
