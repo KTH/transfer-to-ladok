e99058dcf1e322309e58ad64f10f2ec68ed99be7
Carlos Saito <carlos@saito.style>
2023-03-14 10:42:00 +0100

    Sort assignments by grading type first and name second

--- packages/t2l-frontend/src/components/AssignmentSelector.tsx
+++ packages/t2l-frontend/src/components/AssignmentSelector.tsx
@@ -10,11 +10,21 @@ export default function AssignmentSelector({
   value: string;
   onChange: (value: string) => void;
 }) {
+  const sortedAssignments = columns.assignments.slice().sort((a, b) => {
+    if (a.gradingType === "letter_grade") {
+      return -1;
+    }
+    if (b.gradingType === "letter_grade") {
+      return 1;
+    }
+    return a.name.localeCompare(b.name);
+  });
+
   return (
     <select value={value} onChange={(e) => onChange(e.target.value)}>
       <option value="">Select an assignment</option>
       <optgroup label="Assignments">
-        {columns.assignments.map((a) => (
+        {sortedAssignments.map((a) => (
           <option value={a.id}>{a.name}</option>
         ))}
       </optgroup>
