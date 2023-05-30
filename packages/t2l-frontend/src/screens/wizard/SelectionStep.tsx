c58f80443397048ee75c11edad6e6e5ec2717f5a
Carlos Saito <carlos@saito.style>
2023-05-30 16:12:32 +0200

    UI for <SelectionStep />

--- packages/t2l-frontend/src/screens/wizard/SelectionStep.tsx
+++ packages/t2l-frontend/src/screens/wizard/SelectionStep.tsx
@@ -1,12 +1,32 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from "react";
-import { Select, Option } from "@kth/style";
+import { Select, Option, OptionGroup } from "@kth/style";
+import { useAssignments, useSections } from "../../hooks/apiClient";
+import Loading from "../../components/Loading";
 
 interface SelectionStepProps {
   onSubmit: () => void;
 }
 
 export default function SelectionStep({ onSubmit }: SelectionStepProps) {
+  const [selectedAssignment, setSelectedAssignment] = React.useState("");
+  const [selectedLadokModule, setSelectedLadokModule] = React.useState("");
+
+  const ladokModulesQuery = useSections();
+  const canvasAssignmentsQuery = useAssignments();
+
+  if (ladokModulesQuery.isError) {
+    throw ladokModulesQuery.error;
+  }
+
+  if (!ladokModulesQuery.data || !canvasAssignmentsQuery.data) {
+    return (
+      <div>
+        <Loading>Loading...</Loading>
+      </div>
+    );
+  }
+
   return (
     <form onSubmit={onSubmit}>
       <h1>Select assignment and date</h1>
@@ -16,12 +36,20 @@ export default function SelectionStep({ onSubmit }: SelectionStepProps) {
       </p>
       <Select
         name="canvas-assignment"
-        value="1"
-        onChange={() => {}}
+        value=""
+        onChange={(value) => setSelectedAssignment(value)}
         label="Select assignment"
         description="Only letter grades will be transferred to Ladok: A-F grades or P/F"
       >
-        <Option value="1">Assignment 1</Option>
+        <Option value="">Select an assignment</Option>
+        <OptionGroup label="Assignments">
+          {canvasAssignmentsQuery.data.assignments.map((assignment) => (
+            <Option value={assignment.id}>{assignment.name}</Option>
+          ))}
+        </OptionGroup>
+        <OptionGroup label="Other columns">
+          <Option value="final-grade">Total column</Option>
+        </OptionGroup>
       </Select>
       <Select
         name="ladok-module"
@@ -30,7 +58,15 @@ export default function SelectionStep({ onSubmit }: SelectionStepProps) {
         label="Ladok module"
         description="To which module do you want the grades to be transferred"
       >
-        <Option value="1">Assignment 1</Option>
+        <Option value="">Select a Ladok module</Option>
+        {ladokModulesQuery.data.kurstillfalle.map((section) => (
+          <OptionGroup label={`${section.courseCode} - (${section.roundCode})`}>
+            {section.modules.map((module) => (
+              <Option value={module.utbildningsinstans}>{module.name}</Option>
+            ))}
+            <Option value={section.utbildningsinstans}>Final grade</Option>
+          </OptionGroup>
+        ))}
       </Select>
       <h2>Examination date</h2>
       <p>
