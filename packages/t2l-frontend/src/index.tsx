d7425ad63ac04dc290d2e0242b01005f3762ecd6
Carlos Saito <carlos@saito.style>
2023-06-13 09:45:12 +0200

    Uninstall msw

--- packages/t2l-frontend/src/index.tsx
+++ packages/t2l-frontend/src/index.tsx
@@ -3,15 +3,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-// if (process.env.NODE_ENV === "development") {
-//   const { worker } = require("./mocks/browser");
-//   worker.start({
-//     serviceWorker: {
-//       url: "/transfer-to-ladok/mockServiceWorker.js",
-//     },
-//   });
-// }
-
 const domNode = document.getElementById("root");
 const root = createRoot(domNode!);
 root.render(<App />);
