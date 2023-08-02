37e070c4fb88631f99cacfe422288ff863d6dc2c
Emil Stenberg <esandin@gmail.com>
2023-08-02 15:12:00 +0200

    added info icon to summary page

--- packages/t2l-frontend/src/screens/wizard/DoneStep.tsx
+++ packages/t2l-frontend/src/screens/wizard/DoneStep.tsx
@@ -11,43 +11,48 @@ export default function DoneStep({ response, onRestart }: DoneStepProps) {
   const failedResults = response.filter((r) => r.status === "error");
 
   return (
-    <div
-      className={
-        failedResults.length === 0 ? "alert alert-success" : "alert alert-info"
-      }
-      role="alert"
-    >
-      {successfulResults.length === 0 && (
-        <h2>No results were transferred to Ladok</h2>
-      )}
-      {successfulResults.length > 0 && (
-        <h2>
-          {successfulResults.length} results transferred successfully to Ladok
-        </h2>
-      )}
-
-      {failedResults.length > 0 && (
-        <>
-          <p>The following grades were not transferred</p>
-          <table>
-            <thead>
-              <th>Student name</th>
-              <th>Personal number</th>
-              <th>Reason</th>
-            </thead>
-            <tbody>
-              {failedResults.map((r) => (
-                <tr>
-                  <td>{r.student.sortableName}</td>
-                  <td>{r.student.personalNumber}</td>
-                  <td>{r.cause?.message}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
-      )}
-      <button onClick={() => onRestart()}>Start over</button>
+    <div className="c2l2">
+      <div
+        className={
+          failedResults.length === 0
+            ? "alert alert-success"
+            : "alert alert-info"
+        }
+        role="alert"
+      >
+        {successfulResults.length === 0 && (
+          <h2>No results were transferred to Ladok</h2>
+        )}
+        {successfulResults.length > 0 && (
+          <h2>
+            {successfulResults.length} results transferred successfully to Ladok
+          </h2>
+        )}
+      </div>
+      <div>
+        {failedResults.length > 0 && (
+          <>
+            <p>The following grades were not transferred</p>
+            <table>
+              <thead>
+                <th>Student name</th>
+                <th>Personal number</th>
+                <th>Reason</th>
+              </thead>
+              <tbody>
+                {failedResults.map((r) => (
+                  <tr>
+                    <td>{r.student.sortableName}</td>
+                    <td>{r.student.personalNumber}</td>
+                    <td>{r.cause?.message}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        )}
+        <button onClick={() => onRestart()}>Start over</button>
+      </div>
     </div>
   );
 }
