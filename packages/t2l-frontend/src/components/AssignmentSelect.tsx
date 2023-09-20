0e573e492ab3fe7128e71a3d03f59c0ac832b0d3
Emil Stenberg <esandin@gmail.com>
2023-09-20 14:37:17 +0200

    show disabled options for unpublished assignments

--- packages/t2l-frontend/src/components/AssignmentSelect.tsx
+++ packages/t2l-frontend/src/components/AssignmentSelect.tsx
@@ -32,18 +32,24 @@ export default function AssignmentSelect({
       description="Only letter grades will be transferred to Ladok: A-F grades or P/F"
       error={error}
     >
-      <Option value="">Select an assignment</Option>
+      <Option value="" disabled={true}>
+        Select an assignment
+      </Option>
       <OptionGroup label="Assignments">
-        {sortedAssignments
-          .filter((assignment) => assignment.published)
-          .map((assignment) => (
-            <Option key={assignment.id} value={assignment.id}>
-              {assignment.name + ":" + assignment.gradingType.replace("_", " ")}
-            </Option>
-          ))}
+        {sortedAssignments.map((assignment) => (
+          <Option
+            key={assignment.id}
+            value={assignment.id}
+            disabled={!assignment.published}
+          >
+            {assignment.name + ":" + assignment.gradingType.replace("_", " ")}
+          </Option>
+        ))}
       </OptionGroup>
       <OptionGroup label="Other columns">
-        <Option value="total">Total column</Option>
+        <Option value="total" disabled={false}>
+          Total column
+        </Option>
       </OptionGroup>
     </Select>
   );
